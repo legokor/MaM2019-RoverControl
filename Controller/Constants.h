19d0034981db221dc6a64626f437659970fe80fe
udaniel <ugridaniel@gmail.com>
2020-09-15 10:22:29 +0200

    gitignore and constants added

---
+++ Controller/Constants.h
@@ -0,0 +1,27 @@
+#if !defined(CONSTANTS_H)
+#define CONSTANTS_H 
+
+
+//MOTOR PINS 
+#define MOTOR_1_STEP 27
+#define MOTOR_1_DIR 14
+#define MOTOR_2_STEP 12
+#define MOTOR_2_DIR 17
+#define MOTOR_MODE_0 16
+#define MOTOR_MODE_1 4
+#define MOTOR_MODE_2 0
+
+//ADC PIN
+#define BATTERY 34
+
+//SERVO PINS
+#define SERVO_DUMP 25
+#define SERVO_COLL 26
+
+//LED PINS
+#define TIMEOUT_LED 35
+#define LORA_LED 32
+#define WS_LED 33
+
+
+#endif // CONSTANTS_H
